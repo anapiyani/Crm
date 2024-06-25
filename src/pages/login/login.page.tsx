4a3b88406aee0518ee87143f2b85451645704db9
nurkeldi <amirovmarat1969@gmail.com>
2024-06-25 15:25:58 +0500

    Creating login page

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,7 +1,11 @@
 import classes from "./styles.module.scss";
 
 const LoginPage = () => {
-  return <div></div>;
+  return (
+    <div className={classes["login"]}>
+      <div className={classes["login__content"]}>asd</div>
+    </div>
+  );
 };
 
 export default LoginPage;
