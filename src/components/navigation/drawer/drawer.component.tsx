67ae9ff7f4385ede6c5aa1d9bbb3a9c376cd304b
chungus0 <dguhanb@gmail.com>
2024-06-27 14:28:21 +0500

    comments resolved

--- src/components/navigation/drawer/drawer.component.tsx
+++ src/components/navigation/drawer/drawer.component.tsx
@@ -4,42 +4,21 @@ import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
 
 import { List, ListItem } from "@mui/material";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import classes from "./styles.module.scss";
 
 import HomeIcon from "@mui/icons-material/Home";
-import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import GroupsIcon from "@mui/icons-material/Groups";
-import PersonIcon from "@mui/icons-material/Person";
-import ContentCutIcon from "@mui/icons-material/ContentCut";
-import Inventory2Icon from "@mui/icons-material/Inventory2";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import CreditCardIcon from "@mui/icons-material/CreditCard";
-import MailIcon from "@mui/icons-material/Mail";
-import DescriptionIcon from "@mui/icons-material/Description";
-import SettingsIcon from "@mui/icons-material/Settings";
-import InfoIcon from "@mui/icons-material/Info";
-import StoreIcon from "@mui/icons-material/Store";
-import { Home } from "@mui/icons-material";
+
 import TopBar from "../topbar/topbar.component";
 
 const drawerWidth = 256;
 
 interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * Remove this when copying and pasting into your project.
-   */
   window?: () => Window;
 }
 
@@ -107,42 +86,19 @@ export default function ResponsiveDrawer(props: Props) {
     </div>
   );
 
-  // Remove this const when copying and pasting into your project.
   const container =
     window !== undefined ? () => window().document.body : undefined;
 
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      {/* <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Responsive drawer
-          </Typography>
-        </Toolbar>
-      </AppBar> */}
       <TopBar userName="John Doe" />
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+      
         <Drawer
           container={container}
           variant="temporary"
@@ -178,45 +134,6 @@ export default function ResponsiveDrawer(props: Props) {
           {drawer}
         </Drawer>
       </Box>
-      {/* <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
-      </Box> */}
     </Box>
   );
 }
--- src/components/navigation/topbar/topbar.component.tsx
+++ src/components/navigation/topbar/topbar.component.tsx
@@ -1,24 +1,12 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import classes from "./styles.module.scss";
-import Avatar from '@mui/material/Avatar';
-
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import MailIcon from "@mui/icons-material/Mail";
-import RedeemIcon from "@mui/icons-material/Redeem";
-import PublicIcon from "@mui/icons-material/Public";
-import HelpIcon from "@mui/icons-material/Help";// Import your icons here
-import { Badge, BadgeProps } from "@mui/material";
-import { styled } from '@mui/material/styles';
+import {Help, Notifications, Public, Redeem,Mail} from "@mui/icons-material"; //Icons import 
+import { Badge, BadgeProps,Avatar,Typography,Toolbar,IconButton,styled,Box } from "@mui/material"; //Material UI imports
 
 
 
-const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
+const StyledBadge = styled(Badge)<BadgeProps>(() => ({
     '& .MuiBadge-badge': {
       left: 15,
       top: 10,
@@ -26,14 +14,14 @@ const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
     },
   }));
 
-interface TopBarProps {
+interface ITopBarProps {
   userName: string;
 }
 
 
 const topbarWidth = 256;
 
-const TopBar: React.FC<TopBarProps> = ({ userName }) => {
+const TopBar: React.FC<ITopBarProps> = ({ userName }) => {
     const count=10;
     return (
     <Box sx={{ flexGrow: 1 }}>
@@ -53,7 +41,7 @@ const TopBar: React.FC<TopBarProps> = ({ userName }) => {
       }}>
         <IconButton className={classes["topbar__toolbar__icon"]} edge="start" color="inherit" aria-label="icon1">
           <StyledBadge badgeContent={count} max={9} color="success" >
-          <MailIcon className={classes["topbar__toolbar__icon__mail"]} sx={
+          <Mail className={classes["topbar__toolbar__icon__mail"]} sx={
             {
                 fontSize: '2rem',
                 color: 'var(--success-main)',
@@ -64,7 +52,7 @@ const TopBar: React.FC<TopBarProps> = ({ userName }) => {
         </IconButton>
             <IconButton className={classes["topbar__toolbar__icon"]} color="inherit" aria-label="icon2">
             <StyledBadge badgeContent={count} max={9} color="secondary" >
-            <PublicIcon className={classes["topbar__toolbar__icon__public"]}
+            <Public className={classes["topbar__toolbar__icon__public"]}
            sx={
             {
                 fontSize: '2rem',
@@ -78,7 +66,7 @@ const TopBar: React.FC<TopBarProps> = ({ userName }) => {
         
         <IconButton className={classes["topbar__toolbar__icon"]}>
         <StyledBadge badgeContent={count} max={9} color="primary" >
-        <NotificationsIcon className={classes["topbar__toolbar__icon__notification"]} 
+        <Notifications className={classes["topbar__toolbar__icon__notification"]} 
           sx={
             {
                 fontSize: '2rem',
@@ -92,7 +80,7 @@ const TopBar: React.FC<TopBarProps> = ({ userName }) => {
     
         <IconButton className={classes["topbar__toolbar__icon"]}color="inherit" aria-label="icon4">
         <StyledBadge badgeContent={count} max={9} color="error" >
-        <RedeemIcon className={classes["topbar__toolbar__icon__redeem"]} 
+        <Redeem className={classes["topbar__toolbar__icon__redeem"]} 
           sx={
             {
                 fontSize: '2rem',
@@ -108,7 +96,7 @@ const TopBar: React.FC<TopBarProps> = ({ userName }) => {
             width: '3.2rem',
             height: '3.2rem',
             
-        }}/> {/* Step 2 & 3: Add Avatar with styling */}
+        }}/> {}
         <Typography style={{ flexGrow: 1, textAlign: 'right' }} 
         sx={{
             fontFamily: 'Roboto',
@@ -120,7 +108,7 @@ const TopBar: React.FC<TopBarProps> = ({ userName }) => {
         </Typography>
         </div>
         <IconButton className={classes["topbar__toolbar__icon"]} color="inherit" aria-label="icon4">
-          <HelpIcon className={classes["topbar__toolbar__icon__help"]} 
+          <Help className={classes["topbar__toolbar__icon__help"]} 
           sx={
             {
                 fontSize: '2.4rem',
