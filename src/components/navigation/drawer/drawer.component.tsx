9add7481ed86859f99e004f67e6e36555e3e27d7
Yesset04 <yesset.yedres@nu.edu.kz>
2024-06-27 14:50:43 +0500

    msg

--- src/components/navigation/drawer/drawer.component.tsx
+++ src/components/navigation/drawer/drawer.component.tsx
@@ -1,22 +1,40 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  styled,
+  Collapse,
+} from "@mui/material";
 
-import { List, ListItem } from "@mui/material";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Toolbar from "@mui/material/Toolbar";
 import classes from "./styles.module.scss";
+import logo from "@/assets/icons/icon_wise_white.svg";
+import {
+  Home,
+  AttachMoney,
+  Notifications,
+  Groups,
+  Person,
+  ContentCut,
+  Inventory2,
+  ShoppingCart,
+  CreditCard,
+  Mail,
+  Description,
+  Settings,
+  Info,
+  Store,
+  ExpandLess,
+  ExpandMore,
+} from "@mui/icons-material";
 
-import HomeIcon from "@mui/icons-material/Home";
-
-import TopBar from "../topbar/topbar.component";
-
-const drawerWidth = 256;
+const drawerWidth = "25.6rem";
 
 interface Props {
   window?: () => Window;
@@ -26,6 +44,8 @@ export default function ResponsiveDrawer(props: Props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
+  const [open, setOpen] = React.useState<string | null>(null);
+  const [selectedIndex, setSelectedIndex] = React.useState<string | null>(null);
 
   const handleDrawerClose = () => {
     setIsClosing(true);
@@ -36,53 +56,167 @@ export default function ResponsiveDrawer(props: Props) {
     setIsClosing(false);
   };
 
-  const handleDrawerToggle = () => {
-    if (!isClosing) {
-      setMobileOpen(!mobileOpen);
-    }
+  const handleClick = (text: string, index: number) => {
+    setOpen(open === text ? null : text);
+    setSelectedIndex(`${index}`);
   };
 
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        {[
-          "Рабочий стол",
-          "Касса",
-          "Активности",
-          "Клиенты",
-          "Сотрудники",
-          "Услуги",
-          "Склад",
-          "Поставщики",
-          "Карты и скидки",
-          "Рассылка",
-          "Отчеты",
-          "Настройки",
-          "Поддержка",
-          "Маркет",
-        ].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon style={{ color: "#C7DFF7" }}>
-                <HomeIcon />
-                
-              </ListItemIcon>
+  const handleChildClick = (parentIndex: number, childIndex: number) => {
+    setSelectedIndex(`${parentIndex}-${childIndex}`);
+  };
+
+  const items = [
+    { text: "Рабочий стол", icon: <Home /> },
+    { text: "Касса", icon: <AttachMoney /> },
+    { text: "Активности", icon: <Notifications /> },
+    { text: "Клиенты", icon: <Groups /> },
+    {
+      text: "Сотрудники",
+      icon: <Person />,
+      children: [
+        { text: "Найти" },
+        { text: "Добавить" },
+        { text: "График работы" },
+        { text: "Зарплата" },
+        { text: "Отделы" },
+        { text: "Кабинеты" },
+        { text: "Права доступа" },
+        { text: "Посещаемость" },
+        { text: "Рейтинг" },
+      ],
+    },
+    { text: "Услуги", icon: <ContentCut /> },
+    { text: "Склад", icon: <Inventory2 /> },
+    { text: "Поставщики", icon: <ShoppingCart /> },
+    { text: "Карты и скидки", icon: <CreditCard /> },
+    { text: "Рассылка", icon: <Mail /> },
+    { text: "Отчеты", icon: <Description /> },
+    { text: "Настройки", icon: <Settings /> },
+    { text: "Поддержка", icon: <Info /> },
+    { text: "Маркет", icon: <Store /> },
+  ];
+
+  const LogoContainer = styled("div")({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: "1rem 0 1rem 2rem",
+    flexDirection: "row",
+    marginTop: "1.2rem",
+  });
+
+  const LogoImage = styled("img")({
+    height: "3.6rem",
+  });
+
+  const IconContainer = styled(ListItemIcon)({
+    height: "2.4rem",
+    width: "5.6rem",
+    minWidth: "auto",
+    color: "#C7DFF7",
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    "& svg": {
+      fontSize: "2.4rem",
+    },
+  });
+
+  const StyledDrawerPaper = styled(Drawer)(({ theme }) => ({
+    "& .MuiDrawer-paper": {
+      boxSizing: "border-box",
+      width: drawerWidth,
+      backgroundColor: "#0a2744",
+      overflowY: "auto",
+      scrollbarWidth: "none", // For Firefox
+      msOverflowStyle: "none", // For Internet Explorer and Edge
+      "&::-webkit-scrollbar": {
+        display: "none", // For Chrome, Safari, and Opera
+      },
+    },
+  }));
+
+  const renderListItems = (items: any[], parentIndex: number | null = null) => {
+    return items.map((item, index) => {
+      const uniqueIndex =
+        parentIndex !== null ? `${parentIndex}-${index}` : `${index}`;
+
+      return (
+        <React.Fragment key={uniqueIndex}>
+          <ListItem disablePadding className={classes["aba"]}>
+            <ListItemButton
+              selected={selectedIndex === uniqueIndex}
+              onClick={() =>
+                parentIndex === null
+                  ? handleClick(item.text, index)
+                  : handleChildClick(parentIndex, index)
+              }
+              sx={{
+                "&.Mui-selected": {
+                  backgroundColor: "#0B6BCB",
+                  borderRadius: "4px",
+                  color: "#97C3F0",
+                  transition: "background-color 0.5s ease",
+                  "& .MuiListItemIcon-root": {
+                    color: "#fff",
+                  },
+                  "&:hover": {
+                    backgroundColor: "#0B6BCB", // Ensure the background color remains the same on hover
+                  },
+                },
+                color: "#97C3F0",
+              }}
+            >
+              {parentIndex === null && (
+                <IconContainer>{item.icon}</IconContainer>
+              )}
               <ListItemText
-                primary={text}
+                primary={item.text}
                 primaryTypographyProps={{
                   color: "common.white",
                   fontWeight: "400",
                   variant: "body1",
-                  fontSize: "16px",
+                  fontSize: "1.6rem",
+                  padding: "0.4rem 0 0.4rem 0",
                 }}
               />
+              {item.children && parentIndex === null ? (
+                open === item.text ? (
+                  <ExpandLess />
+                ) : (
+                  <ExpandMore />
+                )
+              ) : null}
             </ListItemButton>
-            
           </ListItem>
-        ))}
-      </List>
+          {item.children ? (
+            <Collapse
+              className={classes["collapse"]}
+              in={open === item.text}
+              timeout={1000}
+              unmountOnExit
+              sx={{ transition: "all 0.5s ease-out" }}
+            >
+              <List
+                className={classes["collapse"]}
+                component="div"
+                disablePadding
+              >
+                {renderListItems(item.children, index)}
+              </List>
+            </Collapse>
+          ) : null}
+        </React.Fragment>
+      );
+    });
+  };
+
+  const drawer = (
+    <div>
+      <LogoContainer>
+        <LogoImage src={logo} alt="SuperWise" />
+      </LogoContainer>
+      <List sx={{ padding: "0.8rem" }}>{renderListItems(items)}</List>
     </div>
   );
 
@@ -92,14 +226,19 @@ export default function ResponsiveDrawer(props: Props) {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <TopBar userName="John Doe" />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}/10rem)` },
+          ml: { sm: `${drawerWidth}/10rem` },
+        }}
+      ></AppBar>
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
         aria-label="mailbox folders"
       >
-      
-        <Drawer
+        <StyledDrawerPaper
           container={container}
           variant="temporary"
           open={mobileOpen}
@@ -117,8 +256,8 @@ export default function ResponsiveDrawer(props: Props) {
           }}
         >
           {drawer}
-        </Drawer>
-        <Drawer
+        </StyledDrawerPaper>
+        <StyledDrawerPaper
           className={classes["sidebar"]}
           variant="permanent"
           sx={{
@@ -132,8 +271,16 @@ export default function ResponsiveDrawer(props: Props) {
           open
         >
           {drawer}
-        </Drawer>
+        </StyledDrawerPaper>
       </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: "0.3rem",
+          width: { sm: `calc(100% - ${drawerWidth}/10rem)` },
+        }}
+      ></Box>
     </Box>
   );
 }
