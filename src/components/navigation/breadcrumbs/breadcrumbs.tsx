cb323edc11394c7caf39f607853de29fee369b18
chungus0 <dguhanb@gmail.com>
2024-06-27 17:35:29 +0500

    breadcrumbs for check

---
+++ src/components/navigation/breadcrumbs/breadcrumbs.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Breadcrumbs as MUIbreadcrumbs, Link, ListItemBaseProps, ListItemProps, Typography } from "@mui/material";
+import { NavigateNext } from "@mui/icons-material";
+import {
+    Link as RouterLink,
+    Route,
+    Routes,
+    MemoryRouter,
+    useLocation,
+} from 'react-router-dom';
+
+export default function BreadcrumbsCustom(): JSX.Element {
+    const location = useLocation();
+    let currentLink = '';
+    const pathnames = location.pathname.split('/').filter((x) => x);
+
+    return (
+
+        <MUIbreadcrumbs aria-label="breadcrumb" separator="/" >
+            {pathnames.map((value, index) => {
+                currentLink += `/${value}`;
+                const last = index === pathnames.length - 1;
+                const to = currentLink;
+                return last ? (
+                    <Typography color="var(--primary-500)" fontWeight={'400'} fontSize={"1.6rem"} key={to}>
+                        {value}
+                    </Typography>
+                ) : (
+                    <Link component={RouterLink} to={to} color="secondary" fontWeight={'400'} fontSize={"1.6rem"} key={to}>
+                        {value}
+                    </Link>
+                );
+            })}
+        </MUIbreadcrumbs>
+    );
+}
