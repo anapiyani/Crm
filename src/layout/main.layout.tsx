31130774abcb2f5b772e5469d2c9c58edb8e6227
Yesset04 <yesset.yedres@nu.edu.kz>
2024-06-29 20:09:33 +0500

    table verstka finished

--- src/layout/main.layout.tsx
+++ src/layout/main.layout.tsx
@@ -2,6 +2,7 @@ import { TopBar } from "@/components/";
 import ResponsiveDrawer from "@/components/navigation/drawer/drawer.component";
 import { Outlet } from "react-router-dom";
 import classes from "./styles.module.scss";
+import TableVertical from "@/components/tables/tableVertical/table-vertical";
 
 const MainLayout = () => {
   return (
@@ -12,6 +13,7 @@ const MainLayout = () => {
       <div className={classes["layout__main"]}>
         <TopBar />
         <Outlet />
+        <TableVertical />
       </div>
     </div>
   );
