e5662a914add8ea5f89685f991e3ab4ec78c9566
Yesset04 <yesset.yedres@nu.edu.kz>
2024-07-02 19:15:16 +0500

    responsiveness started

--- src/components/tables/tableVertical/employee-vertical-info-card.tsx
+++ src/components/tables/tableVertical/employee-vertical-info-card.tsx
@@ -52,8 +52,7 @@ const TableVertical: React.FC<TableVerticalProps> = ({
     <TableContainer
       component={Paper}
       sx={{
-        width: "45rem",
-        mb: "5rem",
+        // width: "45rem",
         border: "0.1rem solid",
         borderRadius: "8px",
         borderColor: "#CDD7E1",
