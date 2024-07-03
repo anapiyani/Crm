769d719136b9368f4952a916c07ffe7e6522b570
nurkeldi <amirovmarat1969@gmail.com>
2024-07-04 01:41:03 +0500

    Finishing calendar

--- src/pages/cash-desk/cash-desk.page.tsx
+++ src/pages/cash-desk/cash-desk.page.tsx
@@ -1,10 +1,13 @@
+import YearlyCalendar from "@/components/calendar/yearly-calendar/yearly-calendar";
 import BreadcrumbsCustom from "@/components/navigation/breadcrumbs/breadcrumbs";
+import Calendar from "@/components/calendar/month-calendar/calendar";
 
 const CashDesk = () => {
   return (
     <div className="">
       <BreadcrumbsCustom />
-      adsfads
+      {/* <YearlyCalendar /> */}
+      <Calendar month={8} year={2001} />
     </div>
   );
 };
