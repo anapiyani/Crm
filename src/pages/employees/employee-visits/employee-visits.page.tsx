a0b40f28b5763ac8b0a70928a04ef37c9a24c0b6
Abyl10 <abylay.kairatbek@gmail.com>
2024-07-19 17:52:15 +0500

    fix: error

--- src/pages/employees/employee-visits/employee-visits.page.tsx
+++ src/pages/employees/employee-visits/employee-visits.page.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import Grid from "@mui/material/Unstable_Grid2";
 import classes from "./styles.module.scss";
 import InfoHeader from "@/components/navigation/header/info-header";
-import {
-  employeeTabsData,
-  employeeNameData,
-} from "@/pages/employees/employee-card/data";
+import { employeeTabsData } from "@/pages/employees/employee-card/data";
 import { data } from "@/pages/employees/employee-visits/data";
 import EmployeeVisitsTable from "./visits-table/employee-visits-table";
 
 const EmployeeVisits = () => {
   return (
     <div className={classes["main"]}>
-      <InfoHeader tabsData={employeeTabsData} nameData={{
-        name: "123"
-      }} />
+      <InfoHeader
+        tabsData={employeeTabsData}
+        nameData={{
+          name: "123",
+        }}
+      />
       <Grid
         container
         sx={{
