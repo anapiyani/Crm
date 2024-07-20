3c0759e786c7d117d37c977ae3815dbc350de092
Abyl10 <abylay.kairatbek@gmail.com>
2024-07-21 00:26:09 +0500

    fix: comment removed

--- src/utils/transform-data.ts
+++ src/utils/transform-data.ts
@@ -76,25 +76,6 @@ export const transformSchedulesToFullCalendar = (schedules: ISchedule[]) => {
       });
     });
   });
-  // const events = schedules
-  //   .map((schedule) => {
-  //     return schedule.appointments.map((appointment) => ({
-  //       id: appointment.id,
-  //       title: `${schedule.employee.first_name} ${schedule.employee.last_name}`,
-  //       start: `${appointment.date}T${appointment.start_time}`,
-  //       end: `${appointment.date}T${appointment.end_time}`,
-  //       resourceId: schedule.employee.id,
-  //       phoneNumber: schedule.employee.phone_number,
-  //       status: appointment.status,
-  //     }));
-  //   })
-  //   .flat();
-
-  // const resources = schedules.map((schedule) => ({
-  //   id: schedule.employee.id,
-  //   title: `${schedule.employee.first_name} ${schedule.employee.last_name}`,
-  //   role: schedule.employee.role,
-  // }));
 
   return { events, resources };
 };
