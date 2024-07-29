dead2c81bfee3af21e73d93ca062b4041afdc8a4
anapiyani <anapiyanurkeldi@gmail.com>
2024-07-29 12:33:39 +0500

    Fix: Fixed comments

--- src/components/autocomplete/custom-autocomplete.component.tsx
+++ src/components/autocomplete/custom-autocomplete.component.tsx
@@ -1,81 +1,82 @@
-import React from "react";
-import { Autocomplete, Paper, SxProps, TextField } from "@mui/material";
-import classes from "./styles.module.scss";
-import classNames from "classnames";
+import { Autocomplete, Paper, SxProps, TextField } from '@mui/material'
+import classNames from 'classnames'
+import React from 'react'
+import classes from './styles.module.scss'
 
 interface IOption {
-  label: string;
-  value: number | string;
+	label: string
+	value: number | string
 }
 
 interface ICustomAutoCompleteProps<T extends IOption> {
-  name: string;
-  selectValue: keyof T;
-  options: T[];
-  onChange?: (value: T | null) => void;
-  value?: T | null;
-  label?: string;
-  placeholder?: string;
-  className?: string;
-  size?: "small" | "medium";
-  sx?: SxProps;
+	name: string
+	selectValue: keyof T
+	options: T[]
+	onChange?: (value: T | null) => void
+	value?: T | null
+	label?: string
+	placeholder?: string
+	className?: string
+	size?: 'small' | 'medium'
+	sx?: SxProps
 }
 
 const isString = (item: unknown): item is string => {
-  return typeof item === "string";
-};
+	return typeof item === 'string'
+}
 
 const CustomAutoComplete = <T extends IOption>({
-  name,
-  selectValue,
-  options,
-  label,
-  onChange,
-  value,
-  placeholder,
-  className,
-  size = "medium",
-  sx,
+	name,
+	selectValue,
+	options,
+	label,
+	onChange,
+	value,
+	placeholder,
+	className,
+	size = 'medium',
+	sx,
 }: ICustomAutoCompleteProps<T>): React.ReactElement => {
-  return (
-    <div className={classNames(classes["autocomplete"], className)}>
-      {label && (
-        <label htmlFor={name} className={classes["autocomplete__label"]}>
-          {label}
-        </label>
-      )}
-      <Autocomplete<T>
-        id={name}
-        options={options}
-        value={value}
-        fullWidth
-        size={size}
-        getOptionLabel={(option) => option[selectValue] as string}
-        isOptionEqualToValue={(option, value) => option.value === value.value}
-        PaperComponent={({ children }) => (
-          <Paper sx={{ fontSize: "1.4rem" }}>{children}</Paper>
-        )}
-        sx={{
-          "& .MuiAutocomplete-inputRoot": {
-            fontSize: "1.4rem",
-          },
-          ...sx,
-        }}
-        onChange={(_, newValue) => {
-          if (onChange) {
-            onChange(newValue as T);
-          }
-        }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            placeholder={placeholder}
-            className={classes["autocomplete__textfield"]}
-          />
-        )}
-      />
-    </div>
-  );
-};
+	return (
+		<div className={classNames(classes['autocomplete'], className)}>
+			{label && (
+				<label htmlFor={name} className={classes['autocomplete__label']}>
+					{label}
+				</label>
+			)}
+			<Autocomplete<T>
+				id={name}
+				options={options}
+				value={value}
+				fullWidth
+				size={size}
+				getOptionLabel={option => option[selectValue] as string}
+				getOptionKey={option => option.value as string}
+				isOptionEqualToValue={(option, value) => option.value === value.value}
+				PaperComponent={({ children }) => (
+					<Paper sx={{ fontSize: '1.4rem' }}>{children}</Paper>
+				)}
+				sx={{
+					'& .MuiAutocomplete-inputRoot': {
+						fontSize: '1.4rem',
+					},
+					...sx,
+				}}
+				onChange={(_, newValue) => {
+					if (onChange) {
+						onChange(newValue as T)
+					}
+				}}
+				renderInput={params => (
+					<TextField
+						{...params}
+						placeholder={placeholder}
+						className={classes['autocomplete__textfield']}
+					/>
+				)}
+			/>
+		</div>
+	)
+}
 
-export default CustomAutoComplete;
+export default CustomAutoComplete
--- src/service/kassa/kassa.service.ts
+++ src/service/kassa/kassa.service.ts
@@ -1,93 +1,108 @@
 import {
-  ICashRegister,
-  IEmployeeWalletInfo,
-  IKassaOperations,
-  IPeriodCashRegister,
-  ISalaryPayment,
-  ISearchKassa,
-  IWithdrawal,
-  KassaResponse,
-} from "@/ts/kassa.interface";
-import api from "../api";
-import { IResponseData } from "@/ts/types";
+	ICashRegister,
+	IEmployeeWalletInfo,
+	IKassaOperations,
+	IPeriodCashRegister,
+	ISalaryPayment,
+	ISearchKassa,
+	IWithdrawal,
+	KassaResponse,
+} from '@/ts/kassa.interface'
+import { IResponseData } from '@/ts/types'
+import api from '../api'
 
 export const getOperations = (
-  kassa_transaction?: boolean
+	kassa_transaction?: boolean
 ): Promise<IKassaOperations[]> => {
-  const url = kassa_transaction
-    ? "/operations/?kassa_transaction=true"
-    : "/operations/";
-  return api.get(url).then((res) => res.data);
-};
+	const url = kassa_transaction
+		? '/operations/?kassa_transaction=true'
+		: '/operations/'
+	return api.get(url).then(res => res.data)
+}
 
 export const getCashRegister = (
-  formData: ICashRegister
+	formData: ICashRegister
 ): Promise<IPeriodCashRegister> => {
-  const params = new URLSearchParams();
-  if (!formData.from_date && !formData.to_date) {
-    params.append("today", formData.today.toString());
-  } else {
-    if (formData.from_date) {
-      params.append("from_date", formData.from_date);
-    }
-    if (formData.to_date) {
-      params.append("to_date", formData.to_date);
-    }
-    params.append("today", formData.today.toString());
-  }
+	const params = new URLSearchParams()
+	if (!formData.from_date && !formData.to_date) {
+		params.append('today', formData.today.toString())
+	} else {
+		if (formData.from_date) {
+			params.append('from_date', formData.from_date)
+		}
+		if (formData.to_date) {
+			params.append('to_date', formData.to_date)
+		}
+		params.append('today', formData.today.toString())
+	}
 
-  const url = `/period_cash_register/?${params.toString()}`;
-  return api.get(url).then((res) => res.data);
-};
+	const url = `/period_cash_register/?${params.toString()}`
+	return api.get(url).then(res => res.data)
+}
 
 export const kassaWithdraw = (formData: IWithdrawal): Promise<IWithdrawal> => {
-  return api.post("/kassa_withdrawal/", formData).then((res) => res.data);
-};
+	return api.post('/kassa_withdrawal/', formData).then(res => res.data)
+}
 
 export const kassaDeposit = (formData: IWithdrawal): Promise<IWithdrawal> => {
-  return api.post("/kassa_deposit/", formData).then((res) => res.data);
-};
+	return api.post('/kassa_deposit/', formData).then(res => res.data)
+}
 
 export const searchKassaData = (
-  formData: ISearchKassa
+	formData: ISearchKassa
 ): Promise<IResponseData<KassaResponse[]>> => {
-  const params = new URLSearchParams();
-  if (formData.operation_type) {
-    params.append("operation_type", formData.operation_type.toString());
-  }
-  if (formData.from_amount) {
-    params.append("from_amount", formData.from_amount.toString());
-  }
-  if (formData.to_amount) {
-    params.append("to_amount", formData.to_amount.toString());
-  }
-  if (formData.from_date) {
-    params.append("from_date", formData.from_date);
-  }
-  if (formData.to_date) {
-    params.append("to_date", formData.to_date);
-  }
-  if (formData.money_type) {
-    formData.money_type.forEach((type) => {
-      params.append("money_type", type);
-    });
-  }
-  if (formData.page) {
-    params.append("page", formData.page.toString());
-  }
-  if (formData.page_size) {
-    params.append("page_size", formData.page_size.toString());
-  }
-  const url = `/transactions/list/?${params.toString()}`;
-  return api.get(url).then((res) => res.data);
-};
+	const params = new URLSearchParams()
+	if (formData.operation_type) {
+		params.append('operation_type', formData.operation_type.toString())
+	}
+	if (formData.from_amount) {
+		params.append('from_amount', formData.from_amount.toString())
+	}
+	if (formData.to_amount) {
+		params.append('to_amount', formData.to_amount.toString())
+	}
+	if (formData.from_date) {
+		params.append('from_date', formData.from_date)
+	}
+	if (formData.to_date) {
+		params.append('to_date', formData.to_date)
+	}
+	if (formData.money_type) {
+		formData.money_type.forEach(type => {
+			params.append('money_type', type)
+		})
+	}
+	if (formData.page) {
+		params.append('page', formData.page.toString())
+	}
+	if (formData.page_size) {
+		params.append('page_size', formData.page_size.toString())
+	}
+	const url = `/transactions/list/?${params.toString()}`
+	return api.get(url).then(res => res.data)
+}
 
 export const getEmployeeSalaryWallet = (
-  id: number
+	id: number
 ): Promise<IEmployeeWalletInfo> => {
-  return api.get(`/salary-wallet/?user_id=${id}`).then((res) => res.data);
-};
+	return api.get(`/salary-wallet/?user_id=${id}`).then(res => res.data)
+}
 
 export const salaryPayment = (formData: ISalaryPayment): Promise<any> => {
-  return api.post("/salary-wallet-payment/", formData).then((res) => res.data);
-};
+	return api.post('/salary-wallet-payment/', formData).then(res => res.data)
+}
+
+export const getIndirectCosts = (formData: any): Promise<any> => {
+	const params = new URLSearchParams()
+	if (formData) {
+		if (formData.from_date) {
+			params.append('from_date', formData.from_date)
+		}
+		if (formData.to_date) {
+			params.append('to_date', formData.to_date)
+		}
+	}
+	return api
+		.get(`/indirect_calculations/?${params?.toString()}`)
+		.then(res => res.data)
+}
