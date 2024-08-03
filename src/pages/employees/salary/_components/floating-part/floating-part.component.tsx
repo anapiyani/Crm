6dacd7aee72cde9cd44d8d98e3382ead5d33969b
anapiyani <anapiyanurkeldi@gmail.com>
2024-08-03 14:15:22 +0500

    Feat: added modal

--- src/pages/employees/salary/_components/floating-part/floating-part.component.tsx
+++ src/pages/employees/salary/_components/floating-part/floating-part.component.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import HeaderTemplate from "../MultiStepHeader/MultiStepHeader.component";
-import StepInput from "../stepInput/stepInput.component";
+import StepInput from "../step-input/step-input.component";
 import classes from "./styles.module.scss";
-import { Button, Checkbox, FormControlLabel } from "@mui/material";
+import { Button, Checkbox, Divider, FormControlLabel } from "@mui/material";
 import NiceModal from "@ebay/nice-modal-react";
 import salaryServicesModal from "@/modals/employees/salary-services.modal";
 
@@ -19,21 +19,127 @@ const FloatingPart: React.FC = () => {
     const newService: DevServiceItem = {
       id: newId,
       element: (
-        <div className={classes.services}>
-          <div className={classes.services__employeePersentage}>
-            <div className={classes.services__employeePersentage__info}>
-              <p>% сотрудника</p>
-              <input type="number" /> <span>%</span>
+        <div>
+          <div style={{ display: "flex" }}>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <div
+                className={classes.services__employeePersentage__info}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "15rem",
+                  marginBottom: "1rem",
+                }}
+              >
+                <p style={{ fontSize: "1.6rem" }}>% сотрудника</p>
+                <div style={{ display: "flex", marginBottom: "1rem" }}>
+                  <input
+                    type="text"
+                    placeholder="0"
+                    style={{ width: "12rem", marginRight: "1rem" }}
+                    onChange={(e) => console.log(e.target.value)}
+                  />
+                  <p>руб.</p>
+                </div>
+                <select name="pets" id="pet-select">
+                  <option value="fixed_percent">Фикс. сумма</option>
+                  <option value="service_percent">% от чека</option>
+                  <option value="service_materials_no_discount_percent">
+                    % - МТ без скидк
+                  </option>
+                  <option value="service_materials_discount_percent">
+                    % - МТ со скид
+                  </option>
+                  <option value="service_minus_client_bonuses_materials_percent">
+                    % от (чека за услуги - бонусы клиента - материалы)
+                  </option>
+                </select>
+              </div>
+              <div
+                className={classes.services__employeePersentage__info}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "15rem",
+                  marginBottom: "1rem",
+                }}
+              >
+                <p style={{ fontSize: "1.3rem" }}>Свои клиенты / 1-й чек</p>
+                <select name="pets" id="pet-select">
+                  <option value="service_percent">По умолчанию</option>
+                  <option value="service_materials_no_discount_percent">
+                    Свои клиенты
+                  </option>
+                  <option value="service_materials_discount_percent">
+                    С первого чека
+                  </option>
+                </select>
+              </div>
+              <div
+                className={classes.services__employeePersentage__info}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "15rem",
+                  marginBottom: "1rem",
+                }}
+              >
+                <p style={{ fontSize: "1.3rem" }}>Прайс / чек</p>
+                <select name="pets" id="pet-select">
+                  <option value="service_percent">По умолчанию</option>
+                  <option value="service_materials_no_discount_percent">
+                    По чеку (после всех скидок)
+                  </option>
+                  <option value="service_materials_discount_percent">
+                    По прайсу (без учета скидок)
+                  </option>
+                </select>
+              </div>
+              <div
+                className={classes.services__employeePersentage__info}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "15rem",
+                  marginBottom: "1rem",
+                }}
+              >
+                <p style={{ fontSize: "1.3rem" }}>Материалы какие</p>
+                <select name="pets" id="pet-select">
+                  <option value="service_percent">По умолчанию</option>
+                  <option value="service_percent">
+                    Вычитать все материалы
+                  </option>
+                  <option value="service_materials_no_discount_percent">
+                    Вычитать только "не в чек"
+                  </option>
+                  <option value="service_materials_discount_percent">
+                    Вычитать только "в чек"
+                  </option>
+                </select>
+              </div>
+              <div
+                className={classes.services__employeePersentage__info}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "15rem",
+                  marginBottom: "1rem",
+                }}
+              >
+                <p style={{ fontSize: "1.3rem" }}>Материалы откуда</p>
+                <select name="pets" id="pet-select">
+                  <option value="service_percent">По умолчанию</option>
+                  <option value="service_percent">Вычитать из выручки</option>
+                  <option value="service_materials_no_discount_percent">
+                    Вычитается из доли мастера
+                  </option>
+                </select>
+              </div>
             </div>
-            <div className={classes.services__employeePersentage__info}>
-              <StepInput
-                labelName="Свои клиенты / 1-й чек"
-                placeholder="По умолчанию"
-                isAutoComplete={true}
-                onChange={(value) => console.log(value)}
-              />
-            </div>
+            {/* hier scheße Button für löschen modal */}
           </div>
+          <Divider />
         </div>
       ),
     };
@@ -42,7 +148,7 @@ const FloatingPart: React.FC = () => {
 
   const handleDeleteService = (id: string) => {
     setDevServices((prevDevServices) =>
-      prevDevServices.filter((service) => service.id !== id)
+      prevDevServices.filter((service) => service.id !== id),
     );
   };
 
