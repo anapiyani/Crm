7f6ce85276823617f0f787470588947bb03b3d80
anapiyani <anapiyanurkeldi@gmail.com>
2024-08-20 12:38:19 +0500

    Fix: some error that i didn't change

--- src/pages/employees/salary/_components/floating-part/floating-part.component.tsx
+++ src/pages/employees/salary/_components/floating-part/floating-part.component.tsx
@@ -59,16 +59,6 @@ const FloatingPart: React.FC<FloatingPartProps> = ({ control }) => {
   }, []);
 
   //Zhango's function to traverse from service to parent
-<<<<<<< HEAD
-=======
-  const treeTraverse = (data: IServiceTextProps[], item: IServiceTextProps) => {
-    let result: string[] = [];
-    console.log(item);
-    if (item.parent === null) {
-      result.push(item.serviceName);
-      return result!;
-    }
->>>>>>> 04e6036 (Feat: Modals for Activity view page)
 
   // Zhango's function to create List of services
   const handleListCreate = (data: IServiceTextProps[]) => {
@@ -87,7 +77,7 @@ const FloatingPart: React.FC<FloatingPartProps> = ({ control }) => {
   };
   const treeTraverse = (
     data: IServiceTextProps[],
-    service: IServiceTextProps
+    service: IServiceTextProps,
   ) => {
     let result: string[] = [];
     console.log(service);
@@ -111,12 +101,12 @@ const FloatingPart: React.FC<FloatingPartProps> = ({ control }) => {
   const handleShowNewService = (
     selected: string[] = [], //default values made by Zhango
     cost: string[] = control._defaultValues.services_with_different_percentage!.map(
-      (item) => item?.employee_percentage || ""
+      (item) => item?.employee_percentage || "",
     ),
     option: { label: string; value: string } = {
       label: "Фикс. сумма",
       value: "fixed_percent",
-    }
+    },
   ) => {
     const newId = `devService-${Date.now()}`;
     console.log("NewService" + selected);
@@ -213,14 +203,15 @@ const FloatingPart: React.FC<FloatingPartProps> = ({ control }) => {
                   NiceModal.show(salaryServicesModal, { serviceIds }).then(
                     (res) => {
                       getServicesFromList(res as IServiceTextProps[]);
-                      const newServiceText: IServiceTextProps[] = res as IServiceTextProps[];
+                      const newServiceText: IServiceTextProps[] =
+                        res as IServiceTextProps[];
                       handleShowNewService(
                         handleListCreate(newServiceText),
                         cost,
-                        option
+                        option,
                       );
                       handleDeleteService(newId);
-                    }
+                    },
                   )
                 }
                 style={{ fontSize: "1.4rem" }}
@@ -264,7 +255,7 @@ const FloatingPart: React.FC<FloatingPartProps> = ({ control }) => {
 
   const handleDeleteService = (id: string) => {
     setDevServices((prevDevServices) =>
-      prevDevServices.filter((service) => service.id !== id)
+      prevDevServices.filter((service) => service.id !== id),
     );
   };
 
