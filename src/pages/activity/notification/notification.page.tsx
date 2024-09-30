f7e500d1959c57fea5615fc00b36c9bee96adcea
anapiyani <anapiyanurkeldi@gmail.com>
2024-09-30 18:37:00 +0500

    Feat: Finished template of notification page

--- src/pages/activity/notification/notification.page.tsx
+++ src/pages/activity/notification/notification.page.tsx
@@ -1,12 +1,37 @@
 import BreadcrumbsCustom from "@/components/navigation/breadcrumbs/breadcrumbs";
 import classes from "./styles.module.scss";
-import { Autocomplete, Divider, TextField } from "@mui/material";
+import {
+  Autocomplete,
+  Button,
+  Divider,
+  Pagination,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
 import CustomDatePicker from "@/components/date-picker/date-picker-custom";
 import CustomTimePicker from "@/components/time-picker/time-picker-custom";
 import StepInput from "@/pages/employees/salary/_components/step-input/step-input.component";
 import RoleEmployeeCheckbox from "@/components/role-employee-checkbox/role-employee-checkbox";
+import { Link } from "react-router-dom";
+import { Chat } from "@mui/icons-material";
+
+interface IOption {
+  label: string;
+  value: number;
+}
 
 const NotificationPage = () => {
+  const pageSizeOptions: IOption[] = [
+    { label: "10", value: 10 },
+    { label: "20", value: 20 },
+    { label: "50", value: 50 },
+    { label: "100", value: 100 },
+  ];
+
   return (
     <div className={classes.notification}>
       <div className={classes.notification__header}>
@@ -168,6 +193,97 @@ const NotificationPage = () => {
               автору напоминания, в противном случае - по напоминаниям с типом
               "Посещение" по мастеру.
             </p>
+          </div>
+        </div>
+        <div className={classes.notification__main__content}>
+          <div className={classes.notification__main__content__client}>
+            <p>Клиент</p>
+            <TextField
+              sx={{
+                width: "100%",
+                "& .MuiInputBase-root": {
+                  fontSize: "14px",
+                },
+              }}
+              size="small"
+              placeholder="Имя / ID / телефон / карта"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={classes.notification__main}>
+        <div className={classes.notification__main__search}>
+          <Button variant="outlined">Сбросить</Button>
+          <Button variant="contained">Поиск</Button>
+          <Button variant="contained">Создать напоминание</Button>
+        </div>
+      </div>
+      <div className={classes.notification__main__tables}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>ФИО</TableCell>
+              <TableCell>Контакты</TableCell>
+              <TableCell>Возрасть</TableCell>
+              <TableCell>Дата рождения</TableCell>
+              <TableCell>Должность</TableCell>
+              <TableCell>Действ.</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>1</TableCell>
+              <TableCell>
+                <Link to={`/notifications/`} className={classes.name_link}>
+                  Алимя Алимов
+                </Link>
+              </TableCell>
+              <TableCell>
+                +7 (700) 323 32 12 <br />
+                aigulmarieva1@gmail.com
+              </TableCell>
+              <TableCell>
+                <p>50</p>
+              </TableCell>
+              <TableCell>
+                <p>24.08.1978</p>
+              </TableCell>
+              <TableCell>Тех. персонал</TableCell>
+              <TableCell>
+                <Link to={"/"}>
+                  <Chat />
+                </Link>{" "}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <div className={classes.notification__main__tables__container}>
+          <div className={classes.notification__main__tables__container__row}>
+            <p className={classes.notification__main__tables__container__label}>
+              Показано 10 из 99 записей
+            </p>
+            <div>
+              <div className={classes.tableSettings}>
+                Показывать
+                <select name="pageSize" id="pageSize">
+                  {pageSizeOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+                записей
+              </div>
+            </div>
+            <Pagination
+              count={10}
+              page={1}
+              variant="outlined"
+              shape="rounded"
+              boundaryCount={1}
+              color="primary"
+            />
           </div>
         </div>
       </div>
