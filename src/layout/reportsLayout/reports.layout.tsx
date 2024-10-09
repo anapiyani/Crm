491dab2e36444c7eac7b6987efe4795e9955369d
Yesset04 <yesset.yedres@nu.edu.kz>
2024-10-09 18:31:03 +0500

    reports page first page finished verstka

---
+++ src/layout/reportsLayout/reports.layout.tsx
@@ -0,0 +1,28 @@
+import ReportsDrawer from "@/components/navigation/drawer_reports/reportsDrawer.component";
+import { Outlet } from "react-router-dom";
+import classes from "./styles.module.scss";
+import { useState } from "react";
+
+const ReportsLayout = () => {
+  const [isReportsDrawerOpen, setReportsDrawerOpen] = useState(true);
+
+  const toggleReportsDrawer = () => {
+    setReportsDrawerOpen(!isReportsDrawerOpen);
+  };
+
+  return (
+    <div className={classes["layout"]}>
+      <div className={classes["layout__sidebar"]}>
+        <ReportsDrawer
+          isOpen={isReportsDrawerOpen}
+          handleClose={toggleReportsDrawer}
+        />
+      </div>
+      <div className={classes["layout__main"]}>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default ReportsLayout;
